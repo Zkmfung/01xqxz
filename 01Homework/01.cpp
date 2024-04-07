03555bb3857f89051f35ed14d30bef30de5c1c5f
Zkmfung <zkmfung@gmail.com>
2024-04-07 19:44:27 +0800

    完成一部分person类的架构

---
+++ 01Homework/01.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//person对象
+class person
+{
+public:
+	string name;
+	char gender_M_F_N;
+
+
+private:
+	int age;
+	double weight_kg;
+	double height_cm;
+
+};
+
+//person派生类
+
+
+
+//用虚继承写一个间接派生类
+
+
+
+int main()
+{
+
+	return 0;
+}
